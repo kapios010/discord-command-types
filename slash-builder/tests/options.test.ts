c55f8f80a06bf66eb3a7e034d024d05faa80bb4c
Kapios010 <67257824+Kapios010@users.noreply.github.com>
2025-08-15 01:53:28 +0200

    test: Add vitest and tests but not really
    Also add builder dep to migrator

---
+++ slash-builder/tests/options.test.ts
@@ -0,0 +1,29 @@
+import { describe, expect, expectTypeOf, it, test } from 'vitest'
+import { BooleanOptionBuilder, boolean } from '../src/options/boolean'
+import { DiscordOptionTypes } from '../src/options/common'
+import { SlashCommandBuilder } from '../src/command/command'
+
+
+// I'LL DO TESTS LATER RAHHHHHHH
+describe('Testing common (not choosable) options', () => {
+
+    it('doesn\'t throw an error when all required options are filled out', () => {
+        let a = boolean('dreamberd','bool').build()
+        expect(a).toEqual<typeof a>({
+            type: DiscordOptionTypes.BOOLEAN,
+            name: 'dreamberd',
+            description: 'bool'
+        })
+    })
+
+    test('aaaa', () => {
+        let a = new SlashCommandBuilder('blapo')
+        .setDescription('bleep')
+        .setOptions(() => [
+            boolean('bleere', 'should there be a bird?')
+        ])
+        .onInteraction((options, interaction) => {})
+
+        console.dir(a.data, {depth: null})
+    })
+})
