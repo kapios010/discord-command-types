92b5c7831738a226d83cf007667936cc0a3714eb
Kapios010 <67257824+Kapios010@users.noreply.github.com>
2025-08-15 01:58:24 +0200

    feat(builder): Add command option util types
    Used for command options

--- slash-builder/src/utils/index.ts
+++ slash-builder/src/utils/index.ts
@@ -17,3 +17,13 @@ export type SimplifyMappedType<T> = [T] extends [unknown] ? T : never;
 export type Assume<T, U> = T extends U ? T : U;
 
 export type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends (<T>() => T extends Y ? 1 : 2) ? true : false;
+
+export type Nullable<
+    T extends any,
+    CanBeNull extends boolean,
+> = CanBeNull extends true ? T | null : T;
+
+export type DefaultBool<
+    T extends boolean | undefined,
+    D extends boolean,
+> = T extends undefined ? D : NonNullable<T>;
